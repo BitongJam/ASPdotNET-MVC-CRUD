c1895b163a0e5beb219b821b1326fee74be7464b
BitongJam <jamesortizalter@gmail.com>
2022-11-08 15:59:44 +0800

    Add all files from my computer

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class StudentController : Controller
+    {
+        static string sampleVal = string.Empty;
+        // 1. *************RETRIEVE ALL STUDENT DETAILS ******************
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentDBHandle dbhandle = new StudentDBHandle();
+            ModelState.Clear();
+            ViewBag.Message = sampleVal;
+            return View(dbhandle.GetStudent());
+
+        }
+
+        // 2. *************ADD NEW STUDENT ******************
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(StudentModel smodel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    StudentDBHandle sdb = new StudentDBHandle();
+                    if (sdb.AddStudent(smodel))
+                    {
+                        ViewBag.Message = "Student Details Added Successfully";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch(Exception x)
+            {
+                ViewBag.Message = x.Message;
+                return View();
+            }
+        }
+
+        // 3. ************* EDIT STUDENT DETAILS ******************
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            StudentDBHandle sdb = new StudentDBHandle();
+            return View(sdb.GetStudent().Find(smodel => smodel.Id == id));
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, StudentModel smodel)
+        {
+            try
+            {
+                StudentDBHandle sdb = new StudentDBHandle();
+                sdb.UpdateDetails(smodel);
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // 4. ************* DELETE STUDENT DETAILS ******************
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                StudentDBHandle sdb = new StudentDBHandle();
+                if (sdb.DeleteStudent(id))
+                {
+                    ViewBag.AlertMsg = "Student Deleted Successfully";
+                    sampleVal = "Delete Success";
+                }
+                
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+    }
---
+++ Models/StudentDBHandle.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class StudentDBHandle
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["studentconn"].ToString();
+            con = new SqlConnection(constring);
+        }
+
+        // **************** ADD NEW STUDENT *********************
+        public bool AddStudent(StudentModel smodel)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("pro_addnewstudent", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@name", smodel.Name);
+            cmd.Parameters.AddWithValue("@city", smodel.City);
+            cmd.Parameters.AddWithValue("@address", smodel.Address);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        // ********** VIEW STUDENT DETAILS ********************
+        public List<StudentModel> GetStudent()
+        {
+            connection();
+            List<StudentModel> studentlist = new List<StudentModel>();
+
+            SqlCommand cmd = new SqlCommand("pro_getstudentdetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                studentlist.Add(
+                    new StudentModel
+                    {
+                        Id = Convert.ToInt32(dr["Id"]),
+                        Name = Convert.ToString(dr["Name"]),
+                        City = Convert.ToString(dr["City"]),
+                        Address = Convert.ToString(dr["Address"])
+                    });
+            }
+            return studentlist;
+        }
+
+        // ***************** UPDATE STUDENT DETAILS *********************
+        public bool UpdateDetails(StudentModel smodel)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("pro_updatestudentDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@studentId", smodel.Id);
+            cmd.Parameters.AddWithValue("@studentName", smodel.Name);
+            cmd.Parameters.AddWithValue("@studentCity", smodel.City);
+            cmd.Parameters.AddWithValue("@studentAddress", smodel.Address);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        // ********************** DELETE STUDENT DETAILS *******************
+        public bool DeleteStudent(int id)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("pro_deleteStudent", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@studentId", id);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+    
+}
+}
